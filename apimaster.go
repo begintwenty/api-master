f4451f09b96d0f7a1dd60ffd7eeda0b4378b8961
SimonV0 <90194246+SimonV0@users.noreply.github.com>
2025-03-17 15:11:51 +0100

    Added constructors

--- apimaster.go
+++ apimaster.go
@@ -1,1 +1,43 @@
 package apimaster
+
+type Handler struct {
+	URL           string
+	AuthHeaderKey string
+	APIKeys       []APIKey
+}
+
+func NewHandler(url string, authHeaderKey string, apiKeys []APIKey) Handler {
+	return Handler{
+		URL:           url,
+		AuthHeaderKey: authHeaderKey,
+		APIKeys:       apiKeys,
+	}
+}
+
+type APIKey struct {
+	Value          string
+	RPSLimit       int
+	DailyLimit     int
+	DailyLimitUsed int
+}
+
+func NewAPIKey(value string, rpsLimit int, dailyLimit int, dailyLimitUsed int) APIKey {
+	return APIKey{
+		Value:          value,
+		RPSLimit:       rpsLimit,
+		DailyLimit:     dailyLimit,
+		DailyLimitUsed: dailyLimitUsed,
+	}
+}
+
+func (h *Handler) GetAvailableKeys() APIKey {
+
+}
+
+func (k *APIKey) IncrementDailyLimitUsed(usage int) {
+	k.DailyLimitUsed += usage
+}
+
+func (k *APIKey) CanUseKey() bool {
+	return k.DailyLimitUsed >= k.DailyLimit
+}
